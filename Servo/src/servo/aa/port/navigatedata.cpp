259e1f4a0dafdb30c7236808784b0138c2e8ce91
kwang <rhkdgus3573@naver.com>
2024-11-05 20:40:51 +0900

    feat(Servo): 핸들러 호출 추가

--- Servo/src/servo/aa/port/navigatedata.cpp
+++ Servo/src/servo/aa/port/navigatedata.cpp
@@ -224,6 +224,12 @@ void NavigateData::ReadDataNEvent(ara::com::SamplePtr<deepracer::service::naviga
 {
     auto data = *samplePtr.Get();
     // put your logic
+
+    // NEvent 핸들러가 등록되어 있을시 해당 핸들러는 값과 함께 호출한다.
+    if (m_receiveEventNEventHandler != nullptr)
+    {
+        m_receiveEventNEventHandler(data);
+    }
 }
 
 void NavigateData::SetReceiveEventNEventHandler(
