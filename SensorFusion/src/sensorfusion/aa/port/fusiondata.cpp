345fd7a2ad8fd5a1e561df411bd0369aa9a6c052
jinsolJEONG <config8@naver.com>
2024-11-16 03:12:31 +0900

    fix(lidar, camera, fusion): 로직 수정

--- SensorFusion/src/sensorfusion/aa/port/fusiondata.cpp
+++ SensorFusion/src/sensorfusion/aa/port/fusiondata.cpp
@@ -29,22 +29,6 @@ SvFusionDataSkeletonImpl::SvFusionDataSkeletonImpl(ara::core::InstanceSpecifier 
 {
 }
  
-ara::core::Future<SvFusionDataSkeleton::FMethodOutput> SvFusionDataSkeletonImpl::FMethod()
-{
-    m_logger.LogVerbose() << "FusionData::FMethod::Requested";
-    
-    FMethodOutput response;
-    ara::core::Promise<FMethodOutput> promise;
-    
-    response.camera_data[0] = m_CameraData.camera_data0
-    response.camera_data[1] = m_CameraData.camera_data1;
-    response.lidar_data = m_LidarData.Lidar_data;
-    response.timestamp = m_CameraData.timestamp;
-    
-    promise.set_value(response);
-    return promise.get_future();
-}
- 
 } /// namespace skeleton
 } /// namespace fusiondata
 } /// namespace service
@@ -61,6 +45,12 @@ FusionData::FusionData()
     : m_logger(ara::log::CreateLogger("SSFU", "PORT", ara::log::LogLevel::kVerbose))
     , m_running{false}
 {
+    // 카메라 데이터 크기 초기화
+    m_FEventData.camera_data.resize(2);  
+    // 라이다 데이터 초기화
+    m_FEventData.lidar_data.clear();
+    // 타임스탬프 초기화
+    m_FEventData.timestamp = 0;
 }
  
 FusionData::~FusionData()
@@ -80,12 +70,12 @@ void FusionData::Start()
     if (offer.HasValue())
     {
         m_running = true;
-        m_logger.LogVerbose() << "FusionData::Start::OfferService";
+        m_logger.LogVerbose() << "FusionData::Start::OfferService::성공!!";
     }
     else
     {
         m_running = false;
-        m_logger.LogError() << "FusionData::Start::OfferService::" << offer.Error().Message();
+        m_logger.LogError() << "FusionData::Start::OfferService::실패!!!" << offer.Error().Message();
     }
 }
  
@@ -99,6 +89,55 @@ void FusionData::Terminate()
     // stop offer service
     m_interface->StopOfferService();
     m_logger.LogVerbose() << "FusionData::Terminate::StopOfferService";
+}
+
+// 현재 CEvent 데이터를 갱신하는 함수
+void FusionData::WriteDataFEventCamera(const deepracer::type::CameraDataNode& data)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    m_FEventData.camera_data[0] = data.camera_data0;
+}
+void FusionData::WriteDataFEventLidar(const deepracer::type::LidarDataNode& data)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    m_FEventData.lidar_data = data.lidar_data;
+    m_FEventData.timestamp = data.timestamp;
+}
+
+void FusionData::SendEventFEventCyclic()
+{
+    while (m_running)
+    {
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            auto send = m_interface->FEvent.Send(m_FEventData);
+
+            if (!m_FEventData.camera_data.empty()) {
+                m_logger.LogVerbose() << "m_FEventData.camera_data에 데이터가 삽입됨.";
+            } else {
+                m_logger.LogVerbose() << "m_FEventData.camera_data가 비어 있음.";
+            }
+            if (!m_FEventData.lidar_data.empty()) {
+                m_logger.LogVerbose() << "m_FEventData.lidar_data 삽입됨.";
+            } else {
+                m_logger.LogVerbose() << "m_FEventData.lidar_data 비어 있음.";
+            }
+
+            if (send.HasValue())
+            {
+                m_logger.LogVerbose() << "FusionData::SendEventFEventCyclic::Send";
+                // << m_FEventData.camera_data[0][0]
+                // << m_FEventData.camera_data[1][0]
+                // << m_FEventData.lidar_data[0]
+                // << m_FEventData.timestamp;
+            }
+            else
+            {
+                m_logger.LogError() << "FusionData::SendEventFEventCyclic::Send::실패" << send.Error().Message();
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
  
 } /// namespace port
