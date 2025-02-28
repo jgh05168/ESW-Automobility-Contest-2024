7f35bf6313be0a36d922f737891a4c7f65aea4b8
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-02-28 17:41:05 +0900

    refactor(inference): 코드 수정

--- Inference/src/inference/aa/port/fusiondata.cpp
+++ Inference/src/inference/aa/port/fusiondata.cpp
@@ -216,7 +216,7 @@ void FusionData::ReceiveEventFEventCyclic()
                 }
             }
         }
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
  
